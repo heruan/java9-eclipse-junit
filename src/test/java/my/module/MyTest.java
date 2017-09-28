7806d5ecb23abc025dbf41bf36059d12eafa6f7b
Giovanni Lovato <heruan@aldu.net>
2017-09-28 18:28:51 +0200

    initial commit

---
+++ src/test/java/my/module/MyTest.java
@@ -0,0 +1,13 @@
+package my.module;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+
+}
